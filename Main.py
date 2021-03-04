715e4403cb61b28ea6aa96585ba8a79dec413a7e
Albert-B-B <55802313+Albert-B-B@users.noreply.github.com>
2021-03-04 10:32:19 +0100

    Basic format

--- Main.py
+++ Main.py
@@ -4,3 +4,25 @@ Created on Thu Mar  4 10:23:44 2021
 
 @author: 
 """
+import tkinter as tk
+
+class person:
+    def __init__(self,status,position,color):
+        #
+        self.status = status
+        #Format [x,y]
+        self.position = position
+        self.color = color
+    def move(self):
+        pass
+    def spread(self):
+        pass
+
+people = []
+for i in range(n):
+    people.append(person(0,[0,0],"black"))
+
+#UI
+        
+
+        
