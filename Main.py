7e183b4c9b4b6a964cfed346c373b926ee37653d
Albert-B-B <55802313+Albert-B-B@users.noreply.github.com>
2021-03-04 10:41:43 +0100

    Temp spread

--- Main.py
+++ Main.py
@@ -8,20 +8,30 @@ import tkinter as tk
 
 class person:
     def __init__(self,status,position,color):
-        #
+        #0 for non contaminated and 1 for infected.
         self.status = status
         #Format [x,y]
         self.position = position
         self.color = color
+    def __str__(self):
+        return "Status: {} Position: {} Color: {}".format(self.status,self.position,self.color)
     def move(self):
         pass
-    def spread(self):
-        pass
-
+    def spread(self, folk):
+        #How it spreads from each person
+        spreadType = 0
+        if self.status == 1:
+            for i in folk:
+                if i.status == 1:
+                    i.status = 0
 people = []
-for i in range(n):
-    people.append(person(0,[0,0],"black"))
-
+#Number of people
+numPep = 2
+for i in range(numPep):
+    people.append(person(1,[0,0],"black"))
+print(people[1])
+people[0].spread(people)
+print(people[1])
 #UI
         
 
