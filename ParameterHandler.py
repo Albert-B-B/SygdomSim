9ab29b34b542f6be82be4c0277465ef9683e5015
Albert-B-B <55802313+Albert-B-B@users.noreply.github.com>
2021-04-16 10:58:02 +0200

    Parameter handler comments

--- ParameterHandler.py
+++ ParameterHandler.py
@@ -4,7 +4,7 @@ Created on Tue Mar 23 12:39:29 2021
 
 @author: Mi
 """
-#Contains standard values for settings.
+#Dictionary containing values for settings.
 defaultConfig = {
     "-width" : 500,
     "-height" : 500,
@@ -31,9 +31,11 @@ defaultConfig = {
 config = defaultConfig.copy()
 
 
+#Translates all parameters input into config containing values
 def initializeConfig(args):
     global config
     for i in range(1,len(args),2):
+        #Checks that changes to the config values match in type as to not cause crashes later on
         try:
             if type(defaultConfig[args[i]]) == type(1):
                 config[args[i]] = int(args[i+1])
@@ -43,17 +45,19 @@ def initializeConfig(args):
                 config[args[i]] = str(args[i+1])
             elif type(defaultConfig[args[i]]) == type(True):
                 config[args[i]] = bool(args[i+1])
+        #Handles errors that can occur
         except TypeError:
             print("ERROR: Can not convert " + str(args[i+1]) + "to default type" )
         except:
             print("ERROR: " + str(args[i])+" is not a valid parameter")
-    
+#Return that parameters value
 def getVal(parameter):
     try:
         return config[parameter]
     except:
         print("ERROR: " + str(parameter) + " is not a valid parameter")
-        
+
+#Code to debug parameter handler
 if __name__ == "__main__":
     import sys
     args = sys.argv
